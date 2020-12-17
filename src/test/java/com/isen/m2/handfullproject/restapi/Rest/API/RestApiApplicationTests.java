a3758fef68e2398b7bfd0f9d4c272d60d7b6abbd
Valentin Magnan <>
2020-12-17 17:11:53 +0100

    Upload initialized project with Spring Initializer

---
+++ src/test/java/com/isen/m2/handfullproject/restapi/Rest/API/RestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.isen.m2.handfullproject.restapi.Rest.API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
